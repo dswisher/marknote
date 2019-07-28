7b4d0114c96c831ede8e61c35faec1d8ffe061af
Doug Swisher <doug.swisher@dowjones.com>
2019-07-28 15:43:05 -0500

    Render note as HTML

--- src/Controllers/NoteController.cs
+++ src/Controllers/NoteController.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Markdig;
+using System;
 
 namespace marknote.Controllers
 {
@@ -9,17 +10,24 @@ namespace marknote.Controllers
     {
         public IActionResult Render(string path)
         {
-            string content = Markdown.ToHtml("This is a text with some *emphasis*");
+            string content = Markdown.ToHtml(String.Format("This is a text with some *emphasis*. Path `{0}`.", path));
+
+            ViewData["NoteContent"] = content;
 
             // string content = String.Format("Hello world!  Path='{0}'.", path);
-            byte[] bytes = Encoding.UTF8.GetBytes(content);
+            // byte[] bytes = Encoding.UTF8.GetBytes(content);
 
-            var stream = new MemoryStream(bytes);
-            var fileStreamResult = new FileStreamResult(stream, "text/plain");
+            // var stream = new MemoryStream(bytes);
+            // var mimeType = "text/html";
+            // // var mimeType = "text/plain";
 
-            // fileStreamResult.FileDownloadName = "FileStreamExample.csv";
+            // var fileStreamResult = new FileStreamResult(stream, mimeType);
 
-            return fileStreamResult;
+            // // fileStreamResult.FileDownloadName = "FileStreamExample.csv";
+
+            // return fileStreamResult;
+
+            return View();
         }
     }
 }
