2f7b65eb967ecbc3ac56d61a4f54bb3d6c0bc84e
Doug Swisher <doug.swisher@dowjones.com>
2019-08-04 07:46:43 -0500

    Redirect to todo note

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -12,26 +12,7 @@ namespace marknote.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return Redirect("/note/todo.md");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
